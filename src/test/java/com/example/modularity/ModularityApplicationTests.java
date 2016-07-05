e5331d7f89a81810e4411baa30042ba91bd2f09d
kmarkovych <kmarkovych@luxoft.com>
2016-07-05 18:53:02 +0300

    Empty project

---
+++ src/test/java/com/example/modularity/ModularityApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.modularity;
+
+import com.example.modularity.ModularityApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ModularityApplication.class)
+@WebAppConfiguration
+public class ModularityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
